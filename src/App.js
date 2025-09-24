05e53ff073d6e8dd5907741707355ef0b01b6b8a
vivian wambugu <ythirav@gmail.com>
2025-09-24 22:03:02 +0300

    Modified app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import HeroSection from './components/HeroSection';
+import AfricaImageSection from './components/AfricaImageSection';
+import CropLibrary from './components/CropLibrary';
+import WhyFarmers from './components/WhyFarmers';
+import ChatBox from './components/ChatBox';
+import FeatureStrip from './components/FeatureStrip';
+import Footer from './components/Footer';
+import Signup from './pages/Signup'; 
+import Login from './pages/Login';
+import Landing from './pages/Landing';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        {/* Homepage route */}
+        <Route
+          path="/"
+          element={
+            <>
+              <HeroSection />
+              <AfricaImageSection />
+              <CropLibrary />
+              <WhyFarmers />
+              <ChatBox />
+              <FeatureStrip />
+              <Footer />
+            </>
+          }
+        />
+
+        {/* Signup route */}
+        <Route path="/signup" element={<Signup />} />
+
+        {/* Login route */}
+        <Route path="/login" element={<Login />} />
+
+        <Route path="/landing" element={<Landing />} />
+      </Routes>
+    </Router>
   );
 }
 
