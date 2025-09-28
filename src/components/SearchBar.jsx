8b7db79446cd0b5e939ef3516acbce892bca666e
vivian wambugu <ythirav@gmail.com>
2025-09-28 03:14:47 +0300

    Added searchbar..jsx

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { FaSearch } from 'react-icons/fa';
+import './SearchBar.css';
+
+const SearchBar = ({ city, setCity, fetchWeather }) => {
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter') fetchWeather();
+  };
+
+  return (
+    <div className="search-bar">
+      <input
+        type="text"
+        placeholder="Search city..."
+        value={city}
+        onChange={(e) => setCity(e.target.value)}
+        onKeyPress={handleKeyPress}
+      />
+      <button onClick={fetchWeather}>
+        <FaSearch />
+      </button>
+    </div>
+  );
+};
+
+export default SearchBar;
